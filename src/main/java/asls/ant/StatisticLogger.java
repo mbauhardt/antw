d92b3c5b31de04eee6db3d822494ad6caeea5e69
Marko Bauhardt <mb@datameer.com>
2012-02-04 21:11:40 +0100

    add libs

--- src/main/java/asls/ant/StatisticLogger.java
+++ src/main/java/asls/ant/StatisticLogger.java
@@ -9,54 +9,56 @@ import asls.model.Target;
 
 public class StatisticLogger extends LoggerAdapter {
 
-	boolean _subProjectBuilding = false;
-	private Projects _projects = new Projects();
+    boolean _subProjectBuilding = false;
+    private Projects _projects = new Projects();
 
-	public void buildFinished(BuildEvent event) {
-		if (event.getException() != null) {
-			err(event.getException());
-		}
-	}
+    public void buildFinished(BuildEvent event) {
+        if (event.getException() != null) {
+            err(event.getException());
+            out("BUILD FAILED");
+        } else {
+            out("BUILD SUCCESSFUL");
+        }
 
-	@Override
-	public void buildStarted(BuildEvent event) {
-	}
+    }
 
-	@Override
-	public void messageLogged(BuildEvent event) {
-	}
+    @Override
+    public void buildStarted(BuildEvent event) {
+    }
 
-	@Override
-	public void targetFinished(BuildEvent event) {
-		String projectName = event.getProject().getName();
-		String targetName = event.getTarget().getName();
-		Target target = _projects.get(projectName).getTarget(targetName)
-				.addFinishTime(new Date());
-		out(target.logFormat(), target.logParameter());
-		if (!_subProjectBuilding) {
-			out("");
-		}
-	}
+    @Override
+    public void messageLogged(BuildEvent event) {
+    }
 
-	@Override
-	public void targetStarted(BuildEvent event) {
-		String projectName = event.getProject().getName();
-		String targetName = event.getTarget().getName();
-		_projects.get(projectName).getTarget(targetName)
-				.addStartTime(new Date()).increment();
-		if (!_subProjectBuilding) {
-			out(projectName.toUpperCase() + "." + targetName.toUpperCase());
-		}
-	}
+    @Override
+    public void targetFinished(BuildEvent event) {
+        String projectName = event.getProject().getName();
+        String targetName = event.getTarget().getName();
+        Target target = _projects.get(projectName).getTarget(targetName).addFinishTime(new Date());
+        out(target.logFormat(), target.logParameter());
+        if (!_subProjectBuilding) {
+            out("");
+        }
+    }
 
-	@Override
-	public void subBuildStarted(BuildEvent event) {
-		_subProjectBuilding = true;
-	}
+    @Override
+    public void targetStarted(BuildEvent event) {
+        String projectName = event.getProject().getName();
+        String targetName = event.getTarget().getName();
+        _projects.get(projectName).getTarget(targetName).addStartTime(new Date()).increment();
+        if (!_subProjectBuilding) {
+            out(projectName.toUpperCase() + "." + targetName.toUpperCase());
+        }
+    }
 
-	@Override
-	public void subBuildFinished(BuildEvent event) {
-		_subProjectBuilding = false;
-	}
+    @Override
+    public void subBuildStarted(BuildEvent event) {
+        _subProjectBuilding = true;
+    }
+
+    @Override
+    public void subBuildFinished(BuildEvent event) {
+        _subProjectBuilding = false;
+    }
 
 }
