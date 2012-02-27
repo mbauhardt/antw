4125d4d0a3dc22e30cfdd8520f5e9d762c417217
Marko Bauhardt <mb@datameer.com>
2012-02-27 23:02:20 +0100

    fix scripts

--- src/main/scripts/antw-update.sh
+++ src/main/scripts/antw-update.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 rootDir=~/.antw
 sourceDir=${rootDir}/source
 
-/usr/bin/env pushd ${sourceDir} >/dev/null
-/usr/bin/env git pull
-/usr/bin/env ./gradlew clean installProject
-/usr/bin/env popd >/dev/null
+pushd ${sourceDir} >/dev/null
+git pull
+./gradlew clean installProject
+popd >/dev/null
