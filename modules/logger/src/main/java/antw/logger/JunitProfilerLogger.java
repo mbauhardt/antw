9824f4ddb35d7c20ef72baf556e473ecfe81a924
Marko Bauhardt <mb@datameer.com>
2012-09-07 20:55:12 +0200

    [#2] implement ant logger that logs the profiler messages into a separat file

---
+++ modules/logger/src/main/java/antw/logger/JunitProfilerLogger.java
@@ -0,0 +1,36 @@
+package antw.logger;
+
+import org.apache.tools.ant.BuildEvent;
+
+import antw.common.util.Constants;
+import antw.common.util.StringUtil;
+
+public class JunitProfilerLogger extends LoggerAdapter {
+
+    private int _spacesBefore = Integer.MAX_VALUE;
+
+    @Override
+    public void messageLogged(BuildEvent event) {
+        if (event.getTask() != null) {
+            if ("junit".equals(event.getTask().getTaskType())) {
+                if (event.getPriority() <= org.apache.tools.ant.Project.MSG_INFO) {
+                    String message = event.getMessage();
+                    if (message.contains(Constants.PROFILER_LABEL)) {
+                        printProfilerMessage(event, message);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void printProfilerMessage(BuildEvent event, String message) {
+        message = StringUtil.remove(Constants.PROFILER_LABEL, message, false);
+        int spaces = StringUtil.countFirstWhiteSpaces(message);
+        if (spaces < _spacesBefore) {
+            _spacesBefore = spaces;
+        }
+        message = message.substring(_spacesBefore);
+        out(message);
+    }
+}
