39d90492338f308f3b14bd4416309f9338c84707
Marko Bauhardt <marko.bauhardt@antw.cc>
2012-12-06 19:19:46 +0100

    junit plain csv logger prints test cases in milliseconds (cherry picked from commit 213a31da498c64b45d0c12059fd6b972390bd457)
    Conflicts:
    
    	build.gradle

--- modules/common/src/main/java/antw/common/util/TimeUtil.java
+++ modules/common/src/main/java/antw/common/util/TimeUtil.java
@@ -2,25 +2,50 @@ package antw.common.util;
 
 public class TimeUtil {
 
-    public static String formatTimeDuration(long timeDuration) {
-        StringBuilder builder = new StringBuilder();
-        long hours = timeDuration / (60 * 60 * 1000);
-        long mod = (timeDuration % (60 * 60 * 1000));
-        long minutes = mod / (60 * 1000);
-        mod = mod % (60 * 1000);
-        long seconds = mod / 1000;
+	public static String formatTimeDuration(long timeDuration) {
+		StringBuilder builder = new StringBuilder();
+		long hours = timeDuration / (60 * 60 * 1000);
+		long mod = (timeDuration % (60 * 60 * 1000));
+		long minutes = mod / (60 * 1000);
+		mod = mod % (60 * 1000);
+		long seconds = mod / 1000;
+		mod = mod % 1000;
+		long ms = mod;
 
-        if (hours != 0) {
-            builder.append(hours);
-            builder.append(" hrs, ");
-        }
-        if (minutes != 0) {
-            builder.append(minutes);
-            builder.append(" mins, ");
-        }
-        builder.append(seconds);
-        builder.append(" sec");
-        return builder.toString();
-    }
+		if (hours != 0) {
+			builder.append(hours);
+			builder.append(" hrs, ");
+		}
+		if (minutes != 0) {
+			builder.append(minutes);
+			builder.append(" mins, ");
+		}
+		if (seconds != 0) {
+			builder.append(seconds);
+			builder.append(" sec, ");
+		}
+		builder.append(ms);
+		builder.append(" ms");
+		return builder.toString();
+	}
 
+	public static long convertTimeDuration(String duration) {
+		long millis = 0;
+		String[] splits = duration.split(",");
+		for (String split : splits) {
+			split = split.trim();
+			String time = split.split(" ")[0];
+			String unit = split.split(" ")[1];
+			if (unit.equals("hrs")) {
+				millis = millis + Long.parseLong(time) * 60 * 60 * 1000;
+			} else if (unit.equals("mins")) {
+				millis = millis + Long.parseLong(time) * 60 * 1000;
+			} else if (unit.equals("sec")) {
+				millis = millis + Long.parseLong(time) * 1000;
+			} else if (unit.equals("ms")) {
+				millis = millis + Long.parseLong(time);
+			}
+		}
+		return millis;
+	}
 }
