6a7a8c9e3063eefcf556b45bae572b24a3a37d5e
Marko Bauhardt <marko.bauhardt@antw.cc>
2012-12-07 12:56:51 +0100

    use ms seconds within junit formatter (cherry picked from commit dae742bfe5e9e345e64218c22fc9a3c8456bd468)
    Conflicts:
    
    	build.gradle

--- modules/junit/src/main/java/antw/junit/JUnitStandardFormatter.java
+++ modules/junit/src/main/java/antw/junit/JUnitStandardFormatter.java
@@ -18,56 +18,64 @@ import antw.junit.model.TestSuites;
 import antw.junit.model.TestCase.Status;
 import antw.junit.util.TestUtil;
 
-public class JUnitStandardFormatter extends antw.common.Printer implements JUnitResultFormatter {
+public class JUnitStandardFormatter extends antw.common.Printer implements
+	JUnitResultFormatter {
 
     private TestSuites _testSuites = new TestSuites();
 
     @Override
     public void addError(Test test, Throwable t) {
-        String suiteName = TestUtil.getSuiteName(test);
-        String testCase = TestUtil.getNameOfTestCase(test);
-        _testSuites.get(suiteName).getTest(testCase).setStatus(Status.ERROR).setMessage(t.getMessage());
+	String suiteName = TestUtil.getSuiteName(test);
+	String testCase = TestUtil.getNameOfTestCase(test);
+	_testSuites.get(suiteName).getTest(testCase).setStatus(Status.ERROR)
+		.setMessage(t.getMessage());
     }
 
     @Override
     public void addFailure(Test test, AssertionFailedError t) {
-        String suiteName = TestUtil.getSuiteName(test);
-        String testCase = TestUtil.getNameOfTestCase(test);
-        _testSuites.get(suiteName).getTest(testCase).setStatus(Status.FAILURE).setMessage(t.getMessage());
+	String suiteName = TestUtil.getSuiteName(test);
+	String testCase = TestUtil.getNameOfTestCase(test);
+	_testSuites.get(suiteName).getTest(testCase).setStatus(Status.FAILURE)
+		.setMessage(t.getMessage());
     }
 
     @Override
     public void startTestSuite(JUnitTest unitTest) throws BuildException {
-        TestSuite testSuite = _testSuites.get(unitTest.getName());
-        newLine();
-        out("%-20s %-65s %n", new Object[] { Constants.TEST_SUITE_LABEL, testSuite.getName() });
+	TestSuite testSuite = _testSuites.get(unitTest.getName());
+	newLine();
+	out("%-20s %-65s %n", new Object[] { Constants.TEST_SUITE_LABEL,
+		testSuite.getName() });
     }
 
     @Override
     public void endTestSuite(JUnitTest unitTest) throws BuildException {
-        _testSuites.getTestSuite(unitTest.getName());
+	_testSuites.getTestSuite(unitTest.getName());
     }
 
     @Override
     public void startTest(Test test) {
-        String suiteName = TestUtil.getSuiteName(test);
-        String testCaseName = TestUtil.getNameOfTestCase(test);
-        _testSuites.getTestSuite(suiteName).getTest(testCaseName).setStartTime(new Date());
+	String suiteName = TestUtil.getSuiteName(test);
+	String testCaseName = TestUtil.getNameOfTestCase(test);
+	_testSuites.getTestSuite(suiteName).getTest(testCaseName)
+		.setStartTime(new Date());
     }
 
     @Override
     public void endTest(Test test) {
-        String suiteName = TestUtil.getSuiteName(test);
-        String testCaseName = TestUtil.getNameOfTestCase(test);
-        TestCase testCase = _testSuites.getTestSuite(suiteName).getTest(testCaseName).setFinishTime(new Date());
-        space(4);
-        out("%-20s %-65s %-15s %-15s %-15s %-10s %n", new Object[] { Constants.TEST_CASE_LABEL, testCase.getName(),
-                testCase.getDurationAsString(), testCase.getCategory(), testCase.getStatus(), testCase.getMessage() });
+	String suiteName = TestUtil.getSuiteName(test);
+	String testCaseName = TestUtil.getNameOfTestCase(test);
+	TestCase testCase = _testSuites.getTestSuite(suiteName)
+		.getTest(testCaseName).setFinishTime(new Date());
+	space(4);
+	out("%-20s %-65s %-15s %-15s %-15s %-10s %n", new Object[] {
+		Constants.TEST_CASE_LABEL, "  " + testCase.getName() + "  ",
+		testCase.getDurationAsString(), testCase.getCategory(),
+		testCase.getStatus(), testCase.getMessage() });
     }
 
     @Override
     public void setOutput(OutputStream outputStream) {
-        setOutputPrint(new PrintStream(outputStream));
+	setOutputPrint(new PrintStream(outputStream));
     }
 
     @Override
